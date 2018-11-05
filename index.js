4b03aa9c6a13a0448d5d69a5dd9953cce80ddde5
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2018-11-05 22:11:40 +0300

    Компонент Image

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import products from '~/constants/Products';
+import Image from '~/src/Image';
 
-console.log(products);
+ReactDOM.render(
+  <Image src={products[0]['imageUrl']} width="150" height="150" alt={products[0]['title']} />,
+  document.getElementById('app')
+);
