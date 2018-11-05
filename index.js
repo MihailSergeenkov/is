781474d6ce78a56cb4d25e2b52f2f05f7cfd15ce
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2018-11-05 22:13:56 +0300

    Компонент Price

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import products from '~/constants/Products';
 import Image from '~/src/Image';
 import TextBox from '~/src/TextBox';
+import Price from '~/src/Price';
 
 ReactDOM.render(
-  <TextBox text="Очень вкусные апельсины" />,
+  <Price price={products[0]['price']} />,
   document.getElementById('app')
 );
