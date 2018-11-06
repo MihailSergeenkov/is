4a54f95bd966b795273a7f0ed3eb667dfb225bd8
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2018-11-06 21:17:06 +0300

    Компонент Catalog

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import products from '~/constants/Products';
-import ProductCard from '~/src/ProductCard';
+import Catalog from '~/src/Catalog';
 
 ReactDOM.render(
-  <ProductCard product={products[0]} />,
-  document.getElementById('app1')
-);
-ReactDOM.render(
-  <ProductCard product={products[1]} />,
-  document.getElementById('app2')
-);
-ReactDOM.render(
-  <ProductCard product={products[2]} />,
-  document.getElementById('app3')
+  <Catalog products={products} />,
+  document.getElementById('app')
 );
