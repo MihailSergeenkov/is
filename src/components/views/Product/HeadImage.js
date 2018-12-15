2bb2a5530ee3c1a9cb4b20e13e087ec6a1a587e0
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2018-12-15 14:39:42 +0300

    Исправил замечания

--- src/components/views/Product/HeadImage.js
+++ src/components/views/Product/HeadImage.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class HeadImage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const { image } = this.props;
 
